ca3596f2f1989cc93854b07014aba8090e0a5d70
Kieran Healy <kjhealy@gmail.com>
2019-03-12 16:57:51 -0400

    redraw

--- babcock.r
+++ babcock.r
@@ -1,133 +1,161 @@
-library(dplyr)
-library(ggplot2)
-library(kjhutils)
+library(tidyverse)
+library(ggrepel)
 
-data <- read.csv("data/combined-clean.csv", header=TRUE)
+babcock <- read_csv("data/combined-clean.csv")
 
+library(showtext)
+showtext_auto()
 
-#data %>% group_by(Babcock) %>%
+library(myriad)
+import_myriad()
+import_myriad_semi()
 
-
-#data %>% group_by(Babcock, PubPriv, Rank)
-
-## out <- ggplot(data, aes(y=Rank, x=PubPriv))
-
-
-## out + geom_histogram() + facet_wrap(~Babcock)
+theme_set(theme_myriad_semi())
 
 
-data.unis <- subset(data, Babcock!="None" & Type=="University")
-#data.unis <- droplevels(data)
 
-data.unis <- within(data.unis, {
+## This function is morally wrong but I'm in a rush
+
+short_names <- function(x){
+    x <- stringr::str_trim(x)
+    x <- stringr::str_replace(x, "New York University", "NYU")
+    x <- stringr::str_replace(x, "^University of ", "")
+    x <- stringr::str_replace(x, "Polytechnic Institute", "PI")
+    x <- stringr::str_replace(x, "the Pacific", "U. of the Pacific")
+    x <- stringr::str_replace(x, "Massachusetts Institute of Technology", "MIT")
+    x <- stringr::str_replace(x, "Rutgers New Jersey at New Brunswick", "Rutgers")
+    x <- stringr::str_replace(x, "North Carolina at", "UNC")
+    x <- stringr::str_replace(x, "University at College Station", "")
+    x <- stringr::str_replace(x, "Washington University in St. Louis St.", "WUSTL")
+    x <- stringr::str_replace(x, " University$", "")
+    x <- stringr::str_replace(x, "Institute of Technology", "Tech")
+    x <- stringr::str_replace(x, "The Catholic University of America", "Catholic U. of Am.")
+    x <- stringr::str_replace(x, "^Pennsylvania$", "U Penn")
+    x <- stringr::str_replace(x, "Indiana University at Bloomington", "Indiana")
+    x <- stringr::str_replace(x, "at Ann Arbor", "")
+    x <- stringr::str_replace(x, "Illinois at Urbana-Champaign", "UIUC")
+    x <- stringr::str_replace(x, "Wisconsin at Madison", "Wisconsin")
+    x <- stringr::str_replace(x, "at Twin Cities", "")
+    x <- stringr::str_replace(x, "University", "U.")
+    x <- stringr::str_replace(x, " School of", "")
+    x <- stringr::str_replace(x, "California Tech", "Caltech")
+    x <- stringr::str_replace(x, "California at Los Angeles", "UCLA")
+    x <- stringr::str_replace(x, "California at Santa Barbara", "UCSB")
+    x <- stringr::str_replace(x, "California at Davis", "UC Davis")
+    x <- stringr::str_replace(x, "California at San Diego", "UCSD")
+    x <- stringr::str_replace(x, "Southern California", "USC")
+    x <- stringr::str_replace(x, "California at Irvine", "UC Irvine")
+    x <- stringr::str_replace(x, "LowellLowell", "Lowell")
+    x <- stringr::str_replace(x, "Las Cruces", "")
+    x <- stringr::str_replace(x, "U. Bowling", "U.")
+    x <- stringr::str_replace(x, "Maryland at Baltimore County", "UMBC")
+    x <- stringr::str_replace(x, "North Carolina State U. at Raleigh", "NCSU")
+    x <- stringr::str_replace(x, "College of Environmental Science and Forestry", "CESF")
+    x <- stringr::str_replace(x, "U. at Baton Rouge Baton", "")
+    x <- stringr::str_replace(x, "Southern Illinois U. at Carbondale", "SIU Carbondale")
+    x <- stringr::str_replace(x, "at Provo", "")
+    x <- stringr::str_replace(x, "Missouri U. of Science & Technology", "Missouri Sci & Tech")
+    x
+}
+
+
+data_unis <- subset(babcock, Babcock!="None" & Type=="University")
+data_unis$sname <- short_names(data_unis$School)
+
+data_unis <- within(data_unis, {
     School <- reorder(School, -Rank)
 })
 
 
-data.unis$Dummy <- 1
+data_unis$Dummy <- 1
 
-p <- ggplot(data.unis, aes(x=Dummy, y=School, color=Babcock))
+data_unis$usnwr_grp <- cut_number(data_unis$Rank, 4)
 
-p1 <- p + geom_point(size=2.8) + facet_grid( ~ PubPriv) + xlab("") + xlim(1,1) + guides(color=guide_legend(title="Babcock\nClassification\nin 1911")) + ylab("US News & World Report Ranking in 2014") + theme_bw() + theme(axis.text.y = element_text(size=rel(0.5)), axis.ticks=element_blank(), axis.text.x = element_blank(), panel.grid.major.x = element_blank(), panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank())
+levels(data_unis$usnwr_grp) <- c("USNWR 1-28" , "USNWR 29-62", "USNWR 63-101", "USNWR 102-200")
+
+babcolors <- c("#FDE540", "#66C668", "#2E908B", "#3B5489")
+
+p <- ggplot(data_unis, aes(x=Dummy, y=reorder(sname, -Rank), fill = Babcock, label = sname))
+
+p1 <- p + geom_tile() + facet_wrap( ~ usnwr_grp, nrow = 1, scales = "free_y") +
+    geom_label(fill = "#FFFFFF", alpha = 0.7, size = rel(2.2)) +
+    guides(fill=guide_legend(title="Babcock Class in 1911 (Schools founded after 1911 not shown)", title.position = "top")) +
+    labs(x = NULL, y = NULL,
+         title = "The Persistence of the Old Regime",
+         subtitle = "1911 Babcock Classification and 2014 US News Rankings",
+         caption = "Kieran Healy. http://kieranhealy.org") +
+#    scale_fill_viridis_d(option = "D", direction = -1) +
+    scale_fill_manual(values = babcolors) +
+    theme(strip.text.x = element_text(size = rel(0.9), face = "bold"),
+          axis.ticks=element_blank(),
+          axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          legend.title = element_text(size = rel(0.9)),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank(),
+          legend.position = "top",
+          legend.justification = "left")
 
 
-pdf(file="figures/babcock-universities.pdf", height=12, width=5)
+
+pdf(file="figures/babcock-universities-tile.pdf", height=9, width=6)
 p1
-credit("\nKieran Healy, http://kieranhealy.org")
 dev.off()
 
 ggsave(
-  "figures/babcock-universities.png",
-  width = 4.5,
-  height = 12,
-  dpi = 300
-)
-
-
-
-## p <- ggplot(data.unis, aes(x=Rank, y=School, color=Babcock))
-
-## p1 <- p + geom_point(size=2.5) + facet_grid( ~ PubPriv) + xlab("US News & World Report National University Rank in 2014\n") + xlim(1,max(data.unis$Rank)+5) + guides(color=guide_legend(title="Babcock Classification in 1911")) + ylab("") + theme_bw() + theme(legend.position="top", axis.text.y = element_text(size=rel(0.5)))
-
-
-## pdf(file="figures/babcock-universities.pdf", height=12, width=7)
-## p1
-## credit("\nKieran Healy, http://kieranhealy.org")
-## dev.off()
-
-## ggsave(
-##   "figures/babcock-universities.png",
-##   width = 7,
-##   height = 12,
-##   dpi = 300
-## )
-
-
-
-
-data.coll <- subset(data, Babcock!="None" & Type=="College")
-data.coll$School <- gsub(" College", "", data.coll$School)
-
-data.coll <- within(data.coll, {
-    School <- reorder(School, -Rank)
-})
-
-data.coll$Dummy <- 1
-
-p <- ggplot(data.coll, aes(x=Dummy, y=School, color=Babcock))
-
-
-p1 <- p + geom_point(size=2.8) + xlab("") + xlim(1,1) + guides(color=guide_legend(title="Babcock\nClassification\nin 1911")) + ylab("US News & World Report Ranking in 2014") + theme_bw() + theme(axis.text.y = element_text(size=rel(0.5)), axis.ticks=element_blank(), axis.text.x = element_blank(), panel.grid.major.x = element_blank(), panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank())
-
-
-## p1 <- p + geom_point(size=2.5) + guides(color=guide_legend(title="Babcock Classification in 1911")) + xlab("US News & World Report Liberal Arts Colleges Rank in 2014\n") + ylab("") + theme_bw() + theme(legend.position="top", axis.text.y = element_text(size=rel(0.7)))
-
-pdf(file="figures/babcock-colleges.pdf", height=12, width=3.5, pointsize = 11)
-p1
-credit("\nKieran Healy, http://kieranhealy.org")
-
-dev.off()
-
-ggsave(
-  "figures/babcock-colleges.png",
+    "figures/babcock-universities-tile.png",
   p1,
-  width = 3.5,
-  height = 12,
+  width = 6,
+  height = 9,
   dpi = 300
 )
-
-
 
 
 ### ALl of the Unis
 
-data.allu <- subset(data, Type=="University")
-#data.allu <- droplevels(data)
+data_allu <- subset(babcock, Type=="University")
+data_allu$Babcock <- stringr::str_replace(data_allu$Babcock, "None", "Not Rated/Not Yet Founded")
+#data_allu <- droplevels(data)
 
-data.allu <- within(data.allu, {
+data_allu <- within(data_allu, {
     School <- reorder(School, -Rank)
 })
 
-data.allu$Dummy <- 1
+data_allu$Dummy <- 1
+data_allu$sname <- short_names(data_allu$School)
+data_allu$usnwr_grp <- cut_number(data_allu$Rank, 4)
 
-p <- ggplot(data.allu, aes(x=Dummy, y=School, color=Babcock))
+levels(data_allu$usnwr_grp) <- c("USNWR 1-52" , "USNWR 53-101", "USNWR 101-152", "USNWR 153-200")
 
+p <- ggplot(data_allu, aes(x=Dummy, y=reorder(sname, -Rank), fill = Babcock, label = sname))
 
-p1 <- p + geom_point(size=2.8) + facet_grid( ~ PubPriv) + xlab("") + xlim(1,1) + guides(color=guide_legend(title="Babcock\nClassification\nin 1911")) + ylab("US News & World Report Ranking in 2014") + theme_bw() + theme(axis.text.y = element_text(size=rel(0.5)), axis.ticks=element_blank(), axis.text.x = element_blank(), panel.grid.major.x = element_blank(), panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank())
+p1 <- p + geom_tile() + facet_wrap( ~ usnwr_grp, nrow = 1, scales = "free_y") +
+    geom_label(fill = "#FFFFFF", alpha = 0.9, size = rel(1.8)) +
+    guides(fill=guide_legend(title="Babcock Class in 1911", title.position = "top")) +
+    labs(x = NULL, y = NULL,
+         title = "The Persistence of the Old Regime",
+         subtitle = "1911 Babcock Classification and 2014 US News Rankings",
+         caption = "Kieran Healy. http://kieranhealy.org") +
+    scale_fill_viridis_d(option = "D", direction = -1) +
+#    scale_fill_manual(values = babcolors) +
+    theme(strip.text.x = element_text(size = rel(0.8), face = "bold"),
+          axis.ticks=element_blank(),
+          axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          legend.title = element_text(size = rel(0.9)),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank(),
+          legend.position = "top",
+          legend.justification = "left")
 
-
-
-## p1 <- p + geom_point(size=2.5) + facet_grid( ~ PubPriv) + xlab("US News & World Report National University Rank in 2014\n") + xlim(1,max(data.allu$Rank)+5) + guides(color=guide_legend(title="Babcock 1911")) + ylab("") + theme_bw() + theme(legend.position="top", axis.text.y = element_text(size=rel(0.5)))
-
-
-pdf(file="figures/babcock-all-universities.pdf", height=18, width=5)
+pdf(file="figures/babcock-universities-tile-all.pdf", height=11, width=6)
 p1
-credit("\nKieran Healy, http://kieranhealy.org")
 dev.off()
 
 ggsave(
-  "figures/babcock-all-universities.png",
-  width = 4.75,
-  height = 18,
+    "figures/babcock-universities-tile-all.png",
+  p1,
+  width = 6,
+  height = 11,
   dpi = 300
 )
